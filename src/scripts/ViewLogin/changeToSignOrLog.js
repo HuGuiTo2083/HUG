cd7ad1019b944dc53dbf5fa2083ccca41ddcf118
HuGuiTo2083 <hugoubaldo@gmail.com>
2025-03-14 18:04:33 -0600

    mi primer commit en HUG

--- src/scripts/ViewLogin/changeToSignOrLog.js
+++ src/scripts/ViewLogin/changeToSignOrLog.js
@@ -1,16 +1,70 @@
 
 export function changeToSignOrLog() {
-    const myBtSignOrLog = document.getElementById('myBtSignUp')
+    const myBtSign = document.getElementById('myBtSignUp')
+    const myBtLogin = document.getElementById('myBtLogin')
+
     const myIconLog = document.getElementById('myIconLog')
     const myIconSign = document.getElementById('myIconSign')
-    myBtSignOrLog.addEventListener('click', () => {
+const imgLeft = document.getElementById('left')
+
+    const myInfoLog = document.getElementById('myInfoLog')
+    const myInfoSign = document.getElementById('myInfoSign')
+
+
+   
+
+    //---para cuando se quiere registrar
+    myBtSign.addEventListener('click', () => {
 
         myIconLog.style.transition = '0.2s'
         myIconLog.style.left = '100%'
 
         myIconSign.style.transition = '0.3s'
-        myIconSign.style.transform = 'rotateY(180deg)'
+        imgLeft.style.transform = 'rotateY(180deg)'
+
+        myInfoSign.style.visibility = 'visible'
+        myInfoLog.style.opacity = '0'
+        myInfoSign.style.opacity = '1'
+
+        console.log(myInfoSign)
+
         myIconSign.style.left = '0'
+
     })
+
+   //---- cuando se quiere loggear
+    myBtLogin.addEventListener('click', () => {
+
+        myIconLog.style.transition = '0.3s'
+        myIconLog.style.left = '55%'
+
+        myInfoSign.style.opacity = '0'
+        myInfoLog.style.opacity= '1'
+
+        console.log(myInfoLog)
+
+        myIconSign.style.transition = '0.2s'
+        imgLeft.style.transform = 'rotateY(180deg)'
+        myIconSign.style.left = '-100%'
+    })
+
+
+  const btRegister = document.getElementById('btRegister')
+ const firstView = document.getElementById('myFirstViewRegister')
+const myInfo_items = document.querySelectorAll('#myInfoSign .myInfo-item')
+
+  btRegister.addEventListener('click', ()=>{
+    
+    console.log(myInfo_items)
+     myInfo_items.forEach(item => {
+        item.style.display= 'none'
+     })
+     firstView.style.removeProperty('display')
+     firstView.style.display ='grid'
+
+
+  })
+
+
 }
 
--- src/views/ViewLogin.jsx
+++ src/views/ViewLogin.jsx
@@ -1,30 +1,118 @@
 import '../styles/style_Login.css'
 import iconoLoginCurveado from '../assets/icono_curveado_login.png'
 import { useEffect } from 'react';
+import full_logo_HUG from '../assets/FullLogo_HUG.png'
+
+
 import { changeToSignOrLog } from '../scripts/ViewLogin/changeToSignOrLog';
 
 function ViewLogin() {
-  useEffect(()=>{
-    changeToSignOrLog()
-  })
-  
-  return (
-    <div className='myBody'>
-      <img src={iconoLoginCurveado} className='myCurvedIcon curvedRight' id='myIconLog'>
-      </img>
+    useEffect(() => {
+        changeToSignOrLog()
+    })
 
-      <img src={iconoLoginCurveado} className='myCurvedIcon curvedLeft' id='myIconSign'>
-      </img>
-       
-       <div className='myMsgToSignUp'>
-        Hola
+    return (
+        <div className='myBody'>
+            <div className='myContainerIcon curvedRight' id='myIconLog'>
 
-        <button id='myBtSignUp'>Sign Up</button>
-       </div>
+                <div className='myMsgToSignUpOrLog MsgRight'>
+                    <img src={full_logo_HUG} className='myFullLogo'></img>
+                    ¿No tienes una cuenta?, has click abajo para crear una!
+
+                    <button id='myBtSignUp' className='myBtSignOrLog'>Sign Up</button>
+                </div>
+                <img src={iconoLoginCurveado} className='myCurvedIcon' >
+                </img>
+            </div>
+
+            <div className='myContainerIcon curvedLeft' id='myIconSign'>
+                <div className='myMsgToSignUpOrLog MsgLeft'>
+                    <img src={full_logo_HUG} className='myFullLogo'></img>
+                    ¿Tienes una cuenta?, has click abajo para iniciar sesión!
+
+                    <button id='myBtLogin' className='myBtSignOrLog'>Log In</button>
+                </div>
+                <img src={iconoLoginCurveado} className='myCurvedIcon left' id='left' ></img>
+            </div>
+
+            <div className='myContainerLogSign' id='myInfoLog' >
+                <div className='myInfo'>
+                    <div className='myInfo-item'>
+                        Inicia Sesión
+                    </div>
+                    <div className='myInfo-item'>
+
+                        {/* input de usuario o correo */}
+                        <div className='myInfo-sub-item'>
+                            <label>Correo: </label>
+                            <input type='text' className='myInput' placeholder='Ingresa tu correo...'></input>
+                        </div>
+
+                        {/* input de contraseña */}
+                        <div className='myInfo-sub-item'>
+                        <label>Contraseña: </label>
+                        <input type='password' className='myInput' placeholder='Ingresa tu contraseña...'></input>
+                        </div>
+
+                        {/* boton para iniciar sesion */}
+                        <div className='myInfo-sub-item'>
+                            <button className='myBtStartSession' type='button'>Iniciar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='myContainerLogSign myInvisible' id='myInfoSign'>
+                <div className='myInfo'>
+                    {/* ----- */}
+
+                   
+                    <div className='myInfo-item'>
+                        Regístrate
+                        
+                    </div>
+                    
+                    <div className='myInfo-item'>
+                      
+                        {/* input de usuario o correo */}
+                        <div className='myInfo-sub-item'>
+                            <label>Correo: </label>
+                            <input type='text' className='myInput' placeholder='Ingresa tu correo...'></input>
+                        </div>
+
+                        {/* input de contraseña */}
+                        <div className='myInfo-sub-item'>
+                        <label>Contraseña: </label>
+                        <input type='password' className='myInput' placeholder='Crea una contraseña...'></input>
+                        </div>
+
+                        {/* boton para iniciar sesion */}
+                        <div className='myInfo-sub-item'>
+                            <button className='myBtStartSession btRegister' type='button' id='btRegister'>Registrarte</button>
+                        </div>
+                    </div>
+
+                    <div className='myDNone' id='myFirstViewRegister'>
+                      <div className='myFirstViewRegister-item myDNone'>
+                        Valida tu Correo:
+                      </div>
+                      <div className='myFirstViewRegister-item myDNone'>
+                        <div className='myEmailVerified'>
+                               <input type='text' className='myInputValidated' placeholder='Ingresa Código de Verificación...'></input>
+                        </div>
+                      </div>
+                    </div>
+                        {/* ------ */}
+                </div>
+            </div>
 
 
-    </div>
-  );
+
+
+
+
+        </div>
+    );
 }
 
 export default ViewLogin;
